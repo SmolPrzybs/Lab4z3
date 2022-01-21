f4fc12e4239739a93442501ab5aded78792b6741
SmolPrzybs <maciek970@gmail.com>
2022-01-21 20:07:51 +0100

    Dodaj pliki projektów.

---
+++ Lab4z3/Lab4z3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+void zamiana(int liczba);
+
+
+int main()
+{
+	int liczba;
+	cout << "Podaj liczbe: ";
+	cin >> liczba;
+	cout << liczba << " Postac binarna: ";
+	zamiana(liczba);
+	cout << endl;
+	return 0;
+}
+
+
+void zamiana(int liczba)
+{
+	int i = 0, tab[31];
+	while (liczba)
+	{
+		tab[i++] = liczba % 2;
+		liczba /= 2;
+	}
+	for (int j = i - 1; j >= 0; j--)
+		cout << tab[j];
+}
